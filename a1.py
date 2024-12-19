2808797418f126c0b6a3f42f1ce91bd8e242499c
Aalfaka764 <umahaviel4@gmail.com>
2024-12-19 19:57:52 +0100

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,7 @@
+print("Hello world")
+#  Printing my name 
+print("My name is Aviel")
+#  Printing a fun fact about myself
+print("I love playing video games and football")
+#  Performing and printing a simple calculation
+print(2+3)
